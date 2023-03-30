10f002af23241ce96628f14a64ea47b3c3b5b3e9
xDepcio <olek.drwal@gmail.com>
2023-03-30 19:34:58 +0200

    fiex linting error

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,8 +1,9 @@
-/* eslint disable */
+/* eslint-disable no-unused-vars */
 import type { Session, User } from "next-auth";
 import type { JWT } from "next-auth/jwt";
 
 type UserId = string
+
 declare module 'next-auth/jwt' {
     interface JWT {
         id: UserId
